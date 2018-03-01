e667692dcb9eae01bfe43b0265e125064f2b31ae
geekabhi007 <5h1dabhi@gmail.com>
2018-03-02 01:19:30 +0530

    Added i18n capability to the web skeleton

--- src/test/java/com/example/SpringWebApp/SpringWebAppApplicationTests.java
+++ src/test/java/com/example/SpringWebApp/SpringWebAppApplicationTests.java
@@ -5,13 +5,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.devopsbuddy.SpringWebAppApplication;
 import com.devopsbuddy.web.i18n.I18NService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootConfiguration
+@ContextConfiguration(classes = SpringWebAppApplication.class)
 @WebAppConfiguration
 
 public class SpringWebAppApplicationTests {
