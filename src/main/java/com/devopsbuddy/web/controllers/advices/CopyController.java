38da360b85746b1b2768e442d54358874e22483b
geekabhi007 <5h1dabhi@gmail.com>
2018-03-03 17:02:09 +0530

    Added Responsive UI

--- src/main/java/com/devopsbuddy/web/controllers/advices/CopyController.java
+++ src/main/java/com/devopsbuddy/web/controllers/advices/CopyController.java
@@ -3,12 +3,12 @@ package com.devopsbuddy.web.controllers.advices;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-public class IndexController {
+public class CopyController {
 
-	@RequestMapping("/")
-	public String home() {
-		return "index";
+	@RequestMapping("/about")
+	public String about()
+	{
+		return "copy/about";
 	}
 }
